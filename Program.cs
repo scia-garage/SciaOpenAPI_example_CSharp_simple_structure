297f54a192a13577ae075b92533cdc1caa4935cf
Adam Pelikán <adam.pelikan@projekcnikancelar.cz>
2019-03-07 19:00:32 +0100

    materials

--- Program.cs
+++ Program.cs
@@ -17,13 +17,34 @@ namespace SciaOpenAPI_example_CSS.mat
             env.RunSCIAEngineer(EnvESA80.TEnvESAApp_ShowWindow.eEPShowWindowShow);
             Console.WriteLine($"SEn opened");
 
-            SCIA.OpenAPI.EsaProject proj = env.OpenProject(@"C:\Users\adamp\Desktop\temp\emptyECEN.esa");
+            SCIA.OpenAPI.EsaProject proj = env.OpenProject(@"c:\SCIA\GIThub\SciaOpenAPI_example_CSS.mat\res\empty.esa");
             Console.WriteLine($"Proj opened");
 
-            Guid mat_id;
+            Guid comatid = Guid.NewGuid();
+            proj.Model.CreateMaterial(new SCIA.OpenAPI.Material(comatid, "conc", 0, "C30/37"));
+            Guid stmatid = Guid.NewGuid();
+            proj.Model.CreateMaterial(new SCIA.OpenAPI.Material(stmatid, "steel", 1, "S355"));
+            Guid timatid = Guid.NewGuid();
+            proj.Model.CreateMaterial(new SCIA.OpenAPI.Material(timatid, "timber", 2, "D22"));
+            Guid alumatid = Guid.NewGuid();
+            proj.Model.CreateMaterial(new SCIA.OpenAPI.Material(alumatid, "alu", 3, "EN-AW3003"));
+            Guid mamatid = Guid.NewGuid();
+            proj.Model.CreateMaterial(new SCIA.OpenAPI.Material(mamatid, "masonry", 4, "M1"));
+            Guid omatid = Guid.NewGuid();
+            proj.Model.CreateMaterial(new SCIA.OpenAPI.Material(omatid, "other", 5, "O2"));
+            Console.WriteLine($"Materials created in ADM");
+
+            proj.Model.CreateCrossSection(new SCIA.OpenAPI.CrossSection(Guid.NewGuid(), "conc.rect", stmatid, "HEA260", 6, 0));
+            proj.Model.CreateCrossSection(new SCIA.OpenAPI.CrossSection(Guid.NewGuid(), "steel.HEA", stmatid, "HEA260", 1, 0));
+            Console.WriteLine($"CSSs created in ADM");
+
+            proj.Model.RefreshModel_ToSCIAEngineer();
+            Console.WriteLine($"My model sent to SEn");
 
             Console.WriteLine($"Press key to exit");
             Console.ReadKey();
+
+            proj.CloseProject(EnvESA80.TEnvESAApp_SaveChanges.eEPSaveChangesNo);
 
         }
     }
