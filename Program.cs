708aecf135fdd347713821ec2d29ace31619241d
Adam Pelikán <adam.pelikan@projekcnikancelar.cz>
2019-03-07 16:17:47 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SciaOpenAPI_example_CSS.mat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello!");
+
+            SCIA.OpenAPI.Environment env = new SCIA.OpenAPI.Environment(@"c:\scia\GIT\sen\A\Bin\release32", @".\Temp");
+
+            env.RunSCIAEngineer(EnvESA80.TEnvESAApp_ShowWindow.eEPShowWindowShow);
+            Console.WriteLine($"SEn opened");
+
+            SCIA.OpenAPI.EsaProject proj = env.OpenProject(@"C:\Users\adamp\Desktop\temp\emptyECEN.esa");
+            Console.WriteLine($"Proj opened");
+
+            Guid mat_id;
+
+            Console.WriteLine($"Press key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
