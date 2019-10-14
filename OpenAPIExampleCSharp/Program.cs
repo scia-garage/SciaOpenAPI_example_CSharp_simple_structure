1e04bf7830ed1f1e9b9100d4836d77bf72241def
Jaroslav Broz <j.broz@scia.net>
2019-10-14 13:13:47 +0200

    Added new features of 19.1
    Added creation of line support, line load and constrains for nodal support

--- OpenAPIExampleCSharp/Program.cs
+++ OpenAPIExampleCSharp/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SCIA.OpenAPI.StructureModelDefinition;
+using SCIA.OpenAPI.Results;
+using SCIA.OpenAPI.OpenAPIEnums;
+using Results64Enums;
 
 namespace SciaOpenAPI_example_simple_structure
 {
@@ -10,8 +14,9 @@ namespace SciaOpenAPI_example_simple_structure
     {
         static void Main(string[] args)
         {
+
             //Initialization of OpenAPI environment
-            using (SCIA.OpenAPI.Environment env = new SCIA.OpenAPI.Environment(@"c:\Program Files (x86)\SCIA\Engineer19.0\", @".\SCIATemp", "1.0.0.0"))//path to the location of your installation and temp path for logs)
+            using (SCIA.OpenAPI.Environment env = new SCIA.OpenAPI.Environment(@"c:\Program Files (x86)\SCIA\Engineer19.1\", @".\SCIATemp", "1.0.0.0"))// path to the location of your installation and temp path for logs)
             {
                 //Run SCIA Engineer application
                 bool openedSE = env.RunSCIAEngineer(SCIA.OpenAPI.Environment.GuiMode.ShowWindowShow);
@@ -33,18 +38,19 @@ namespace SciaOpenAPI_example_simple_structure
                 Console.WriteLine($"Set grade for concrete material: ");
                 string conMatGrade = Console.ReadLine();
                 Guid comatid = Guid.NewGuid();
-                proj.Model.CreateMaterial(new SCIA.OpenAPI.StructureModelDefinition.Material(comatid, "conc", 0, conMatGrade));
+                proj.Model.CreateMaterial(new Material(comatid, "conc", 0, conMatGrade));
+                //Guid test = proj.Model.FindMaterialGuid(conMatGrade);
                 Console.WriteLine($"Set grade for steel material: ");
                 string steelMatGrade = Console.ReadLine();
                 Guid stmatid = Guid.NewGuid();
-                proj.Model.CreateMaterial(new SCIA.OpenAPI.StructureModelDefinition.Material(stmatid, "steel", 1, steelMatGrade));
+                proj.Model.CreateMaterial(new Material(stmatid, "steel", 1, steelMatGrade));
                 Console.WriteLine($"Materials created in ADM");
                 //Create cross-sections in local ADM
-                //proj.Model.CreateCrossSection(new SCIA.OpenAPI.StructureModelDefinition.CrossSectionParametric(Guid.NewGuid(), "conc.rect", comatid, 1, new double[] { 0.2, 0.4 }));//example of parametric CSS - rectangle
+                //proj.Model.CreateCrossSection(new CrossSectionParametric(Guid.NewGuid(), "conc.rect", comatid, 1, new double[] { 0.2, 0.4 }));//example of parametric CSS - rectangle
                 Guid css_steel = Guid.NewGuid();
                 Console.WriteLine($"Set steel profile: ");
                 string steelProfile = Console.ReadLine();
-                proj.Model.CreateCrossSection(new SCIA.OpenAPI.StructureModelDefinition.CrossSectionManufactured(css_steel, "steel.HEA", stmatid, steelProfile, 1, 0));
+                proj.Model.CreateCrossSection(new CrossSectionManufactured(css_steel, "steel.HEA", stmatid, steelProfile, 1, 0));
                 Console.WriteLine($"CSSs created in ADM");
 
                 Console.WriteLine($"Set parameter a: ");
@@ -63,141 +69,205 @@ namespace SciaOpenAPI_example_simple_structure
                 Guid n7 = Guid.NewGuid();
                 Guid n8 = Guid.NewGuid();
                 //Create structural nodes in local ADM
-                proj.Model.CreateNode(new SCIA.OpenAPI.StructureModelDefinition.StructNode(n1, "n1", 0, 0, 0));
-                proj.Model.CreateNode(new SCIA.OpenAPI.StructureModelDefinition.StructNode(n2, "n2", a, 0, 0));
-                proj.Model.CreateNode(new SCIA.OpenAPI.StructureModelDefinition.StructNode(n3, "n3", a, b, 0));
-                proj.Model.CreateNode(new SCIA.OpenAPI.StructureModelDefinition.StructNode(n4, "n4", 0, b, 0));
-                proj.Model.CreateNode(new SCIA.OpenAPI.StructureModelDefinition.StructNode(n5, "n5", 0, 0, c));
-                proj.Model.CreateNode(new SCIA.OpenAPI.StructureModelDefinition.StructNode(n6, "n6", a, 0, c));
-                proj.Model.CreateNode(new SCIA.OpenAPI.StructureModelDefinition.StructNode(n7, "n7", a, b, c));
-                proj.Model.CreateNode(new SCIA.OpenAPI.StructureModelDefinition.StructNode(n8, "n8", 0, b, c));
+                proj.Model.CreateNode(new StructNode(n1, "n1", 0, 0, 0));
+                proj.Model.CreateNode(new StructNode(n2, "n2", a, 0, 0));
+                proj.Model.CreateNode(new StructNode(n3, "n3", a, b, 0));
+                proj.Model.CreateNode(new StructNode(n4, "n4", 0, b, 0));
+                proj.Model.CreateNode(new StructNode(n5, "n5", 0, 0, c));
+                proj.Model.CreateNode(new StructNode(n6, "n6", a, 0, c));
+                proj.Model.CreateNode(new StructNode(n7, "n7", a, b, c));
+                proj.Model.CreateNode(new StructNode(n8, "n8", 0, b, c));
 
                 Guid b1 = Guid.NewGuid();
                 Guid b2 = Guid.NewGuid();
                 Guid b3 = Guid.NewGuid();
                 Guid b4 = Guid.NewGuid();
                 //Create beams in local ADM
-                proj.Model.CreateBeam(new SCIA.OpenAPI.StructureModelDefinition.Beam(b1, "b1", css_steel, new Guid[2] { n1, n5 }));
-                proj.Model.CreateBeam(new SCIA.OpenAPI.StructureModelDefinition.Beam(b2, "b2", css_steel, new Guid[2] { n2, n6 }));
-                proj.Model.CreateBeam(new SCIA.OpenAPI.StructureModelDefinition.Beam(b3, "b3", css_steel, new Guid[2] { n3, n7 }));
-                proj.Model.CreateBeam(new SCIA.OpenAPI.StructureModelDefinition.Beam(b4, "b4", css_steel, new Guid[2] { n4, n8 }));
+                proj.Model.CreateBeam(new Beam(b1, "b1", css_steel, new Guid[2] { n1, n5 }));
+                proj.Model.CreateBeam(new Beam(b2, "b2", css_steel, new Guid[2] { n2, n6 }));
+                proj.Model.CreateBeam(new Beam(b3, "b3", css_steel, new Guid[2] { n3, n7 }));
+                proj.Model.CreateBeam(new Beam(b4, "b4", css_steel, new Guid[2] { n4, n8 }));
                 //Create fix nodal support in local ADM
-                proj.Model.CreatePointSupport(new SCIA.OpenAPI.StructureModelDefinition.PointSupport(Guid.NewGuid(), "Su1", n1));
-                proj.Model.CreatePointSupport(new SCIA.OpenAPI.StructureModelDefinition.PointSupport(Guid.NewGuid(), "Su2", n2));
-                proj.Model.CreatePointSupport(new SCIA.OpenAPI.StructureModelDefinition.PointSupport(Guid.NewGuid(), "Su3", n3));
-                proj.Model.CreatePointSupport(new SCIA.OpenAPI.StructureModelDefinition.PointSupport(Guid.NewGuid(), "Su4", n4));
-
+                var Su1 = new PointSupport(Guid.NewGuid(), "Su1", n1)
+                {
+                    ConstraintRx = eConstraintType.Free,
+                    ConstraintRy = eConstraintType.Free,
+                    ConstraintRz = eConstraintType.Free
+                };
+                proj.Model.CreatePointSupport(Su1);
+                proj.Model.CreatePointSupport(new PointSupport(Guid.NewGuid(), "Su2", n2));
+                proj.Model.CreatePointSupport(new PointSupport(Guid.NewGuid(), "Su3", n3));
+                proj.Model.CreatePointSupport(new PointSupport(Guid.NewGuid(), "Su4", n4));
 
                 Guid s1 = Guid.NewGuid();
                 Guid[] nodes = new Guid[4] { n5, n6, n7, n8 };
                 Console.WriteLine($"Set thickness of the slab: ");
                 double thickness = Convert.ToDouble(Console.ReadLine());
                 //Create flat slab in local ADM
-                proj.Model.CreateSlab(new SCIA.OpenAPI.StructureModelDefinition.Slab(s1, "s1", 0, comatid, thickness, nodes));
+                string slabName = "SLAB_1";
+                proj.Model.CreateSlab(new Slab(s1, slabName, 0, comatid, thickness, nodes));
+
+
 
                 Guid lg1 = Guid.NewGuid();
                 //Create load group in local ADM
-                proj.Model.CreateLoadGroup(new SCIA.OpenAPI.StructureModelDefinition.LoadGroup(lg1, "lg1", 0));
+                proj.Model.CreateLoadGroup(new LoadGroup(lg1, "lg1", 0));
 
                 Guid lc1 = Guid.NewGuid();
                 //Create load case in local ADM
-                proj.Model.CreateLoadCase(new SCIA.OpenAPI.StructureModelDefinition.LoadCase(lc1, "lc1", 0, lg1, 1));
+                proj.Model.CreateLoadCase(new LoadCase(lc1, "lc1", 0, lg1, 1));
+
+                //Combination
+                CombinationItem[] combinationItems = new CombinationItem[1] { new CombinationItem(lc1, 1.5) };
+                Combination C1 = new Combination(Guid.NewGuid(), "C1", combinationItems)
+                {
+                    Category = eLoadCaseCombinationCategory.AccordingNationalStandard,
+                    NationalStandard = eLoadCaseCombinationStandard.EnUlsSetC
+                };
+                proj.Model.CreateCombination(C1);
 
                 Guid sf1 = Guid.NewGuid();
                 Console.WriteLine($"Set value of surface load on the slab: ");
                 double loadValue = Convert.ToDouble(Console.ReadLine());
                 //Create surface load on slab in local ADM
-                proj.Model.CreateSurfaceLoad(new SCIA.OpenAPI.StructureModelDefinition.SurfaceLoad(sf1, "sf1", loadValue, lc1, s1, 2));
+                proj.Model.CreateSurfaceLoad(new SurfaceLoad(sf1, "sf1", loadValue, lc1, s1, 2));
+                // line support on B1
+                var lineSupport = new LineSupport(Guid.NewGuid(), "lineSupport", b1)
+                {
+                    Member = b1,
+                    ConstraintRx = eConstraintType.Free,
+                    ConstraintRy = eConstraintType.Free,
+                    ConstraintRz = eConstraintType.Free
+                };
+                proj.Model.CreateLineSupport(lineSupport);
+                //line load on B1
+                var lineLoad = new LineLoadOnBeam(Guid.NewGuid(), "lineLoad")
+                {
+                    Member = b1,
+                    LoadCase = lc1,
+                    Value1 = -12500,
+                    Value2 = -12500,
+                    Direction = eDirection.X
+                };
+                proj.Model.CreateLineLoad(lineLoad);
 
                 //Refresh model in SCIA Engineer from local ADM
                 proj.Model.RefreshModel_ToSCIAEngineer();
                 Console.WriteLine($"My model sent to SEn");
 
 
-                //proj.CreateMesh(); //needs dialogue click
                 // Run calculation
                 proj.RunCalculation();
                 Console.WriteLine($"My model calculate");
 
                 //Initialize Results API
-                SCIA.OpenAPI.Results.ResultsAPI rapi = proj.Model.InitializeResultsAPI();
-                //Create container for 1D results
-                SCIA.OpenAPI.Results.Result IntFor1Db1 = new SCIA.OpenAPI.Results.Result();
-                //Results key for internal forces on beam 1
-                SCIA.OpenAPI.Results.ResultKey keyIntFor1Db1 = new SCIA.OpenAPI.Results.ResultKey
+                ResultsAPI rapi = proj.Model.InitializeResultsAPI();
+                if (rapi != null)
                 {
-                    CaseType = Results64Enums.eDsElementType.eDsElementType_LoadCase,
-                    CaseId = lc1,
-                    EntityType = Results64Enums.eDsElementType.eDsElementType_Beam,
-                    EntityName = "b1",
-                    Dimension = Results64Enums.eDimension.eDim_1D,
-                    ResultType = Results64Enums.eResultType.eFemBeamInnerForces,
-                    CoordSystem = Results64Enums.eCoordSystem.eCoordSys_Local
-                };
-                //Load 1D results based on results key
-                IntFor1Db1 = rapi.LoadResult(keyIntFor1Db1);
-                Console.WriteLine(IntFor1Db1.GetTextOutput());
-                var N = IntFor1Db1.GetMagnitudeName(0);
-                var Nvalue = IntFor1Db1.GetValue(0, 0);
-                Console.WriteLine(N);
-                Console.WriteLine(Nvalue);
-                //Results key for reaction on node 1
-                SCIA.OpenAPI.Results.ResultKey keyReactionsSu1 = new SCIA.OpenAPI.Results.ResultKey
-                {
-                    CaseType = Results64Enums.eDsElementType.eDsElementType_LoadCase,
-                    CaseId = lc1,
-                    EntityType = Results64Enums.eDsElementType.eDsElementType_Node,
-                    EntityName = "n1",
-
-                    Dimension = Results64Enums.eDimension.eDim_reactionsPoint,
-                    ResultType = Results64Enums.eResultType.eReactionsNodes,
-                    CoordSystem = Results64Enums.eCoordSystem.eCoordSys_Global
-                };
-                SCIA.OpenAPI.Results.Result reactionsSu1 = new SCIA.OpenAPI.Results.Result();
-                reactionsSu1 = rapi.LoadResult(keyReactionsSu1);
-                Console.WriteLine(reactionsSu1.GetTextOutput());
-
-
-
-                SCIA.OpenAPI.Results.Result Def2Ds1 = new SCIA.OpenAPI.Results.Result();
-                //Results key for internal forces on slab
-                SCIA.OpenAPI.Results.ResultKey keyDef2Ds1 = new SCIA.OpenAPI.Results.ResultKey
-                {
-                    CaseType = Results64Enums.eDsElementType.eDsElementType_LoadCase,
-                    CaseId = lc1,
-                    EntityType = Results64Enums.eDsElementType.eDsElementType_Slab,
-                    EntityName = "s1",
-                    Dimension = Results64Enums.eDimension.eDim_2D,
-                    ResultType = Results64Enums.eResultType.eFemDeformations,
-                    CoordSystem = Results64Enums.eCoordSystem.eCoordSys_Local
-                };
-
-                Def2Ds1 = rapi.LoadResult(keyDef2Ds1);
-                Console.WriteLine(Def2Ds1.GetTextOutput());
-
-                double maxvalue = 0;
-                double pivot;
-                for (int i = 0; i < Def2Ds1.GetMeshElementCount(); i++)
-                {
-                    pivot = Def2Ds1.GetValue(2, i);
-                    if (System.Math.Abs(pivot) > System.Math.Abs(maxvalue))
+                    //Create container for 1D results
+                    Result IntFor1Db1 = new Result();
+                    //Results key for internal forces on beam 1
+                    ResultKey keyIntFor1Db1 = new ResultKey
                     {
-                        maxvalue = pivot;
-
+                        CaseType = eDsElementType.eDsElementType_LoadCase,
+                        CaseId = lc1,
+                        EntityType = eDsElementType.eDsElementType_Beam,
+                        EntityName = "b1",
+                        Dimension = eDimension.eDim_1D,
+                        ResultType = eResultType.eFemBeamInnerForces,
+                        CoordSystem = eCoordSystem.eCoordSys_Local
                     };
-                };
-                Console.WriteLine("Maximum deformation on slab:");
-                Console.WriteLine(maxvalue);
+                    //Load 1D results based on results key
+                    IntFor1Db1 = rapi.LoadResult(keyIntFor1Db1);
+                    if (IntFor1Db1 != null)
+                    {
+                        Console.WriteLine(IntFor1Db1.GetTextOutput());
+                    }
+                    //combination
+                    //Create container for 1D results
+                    Result IntFor1Db1Combi = new Result();
+                    //Results key for internal forces on beam 1
+                    ResultKey keyIntFor1Db1Combi = new ResultKey
+                    {
+                        EntityType = eDsElementType.eDsElementType_Beam,
+                        EntityName = "b1",
+                        CaseType = eDsElementType.eDsElementType_Combination,
+                        CaseId = C1.Id,
+                        Dimension = eDimension.eDim_1D,
+                        ResultType = eResultType.eFemBeamInnerForces,
+                        CoordSystem = eCoordSystem.eCoordSys_Local
+                    };
+                    // Load 1D results based on results key
+                    IntFor1Db1Combi = rapi.LoadResult(keyIntFor1Db1Combi);
+                    if (IntFor1Db1Combi != null)
+                    {
+                        Console.WriteLine(IntFor1Db1Combi.GetTextOutput());
+                    }
+                    ResultKey keyReactionsSu1 = new ResultKey
+                    {
+                        CaseType = eDsElementType.eDsElementType_LoadCase,
+                        CaseId = lc1,
+                        EntityType = eDsElementType.eDsElementType_Node,
+                        EntityName = "n1",
+                        Dimension = eDimension.eDim_reactionsPoint,
+                        ResultType = eResultType.eReactionsNodes,
+                        CoordSystem = eCoordSystem.eCoordSys_Global
+                    };
+                    Result reactionsSu1 = new Result();
+                    reactionsSu1 = rapi.LoadResult(keyReactionsSu1);
+                    if (reactionsSu1 != null)
+                    {
+                        Console.WriteLine(reactionsSu1.GetTextOutput());
+                    }
 
 
+                    Result Def2Ds1 = new Result();
+                    //Results key for internal forces on slab
+                    ResultKey keySlab = new ResultKey
+                    {
+                        EntityType = eDsElementType.eDsElementType_Slab,
+                        EntityName = slabName,
+                        CaseType = eDsElementType.eDsElementType_LoadCase,
+                        CaseId = lc1,
+                        Dimension = eDimension.eDim_2D,
+                        ResultType = eResultType.eFemDeformations,
+                        CoordSystem = eCoordSystem.eCoordSys_Local
+                    };
 
+
+                    Def2Ds1 = rapi.LoadResult(keySlab);
+                    if (Def2Ds1 != null)
+                    {
+                        Console.WriteLine(Def2Ds1.GetTextOutput());
+
+                        double maxvalue = 0;
+                        double pivot;
+                        for (int i = 0; i < Def2Ds1.GetMeshElementCount(); i++)
+                        {
+                            pivot = Def2Ds1.GetValue(2, i);
+                            if (System.Math.Abs(pivot) > System.Math.Abs(maxvalue))
+                            {
+                                maxvalue = pivot;
+
+                            };
+                        };
+                        Console.WriteLine("Maximum deformation on slab:");
+                        Console.WriteLine(maxvalue);
+                    }
+
+                }
 
                 Console.WriteLine($"Press key to exit");
                 Console.ReadKey();
 
                 proj.CloseProject(SCIA.OpenAPI.SaveMode.SaveChangesNo);
+                env.Dispose();
             }
+
         }
+
+
     }
 }
+
 
